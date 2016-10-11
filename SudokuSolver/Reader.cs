1ae2b4606fa2d8e10271c6b43eacc909c9420aeb
Sidhant Mishra <sidhant@oncorp.com>
2016-10-11 17:12:41 -0400

    Init Commit

---
+++ SudokuSolver/Reader.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.IO;
+
+namespace SudokuSolver
+{
+    class Reader
+    {
+        
+        static void Main(string[] args)
+        {
+            var directory = ConfigurationManager.AppSettings["SudokuPath"];
+            Console.WriteLine("Reading Sudoku files from path: {0}", directory);
+            var files = Directory.GetFiles(directory, "*.txt");
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+                if(ValidateFile(file))
+                {
+                    Console.WriteLine("good");
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static bool ValidateFile(string file)
+        {
+            var fileContent = File.ReadAllLines(file);
+            if (fileContent.Length == 9)
+            {
+                foreach (var line in fileContent)
+                {
+                    if (line.Length!= 9)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
