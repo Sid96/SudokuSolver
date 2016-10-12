1feb6dbece723069af9cb75fbfc7cd00e5d6bc8d
Sidhant Mishra <sidhant@oncorp.com>
2016-10-12 09:54:42 -0400

    Implemented functions

--- SudokuSolver/Reader.cs
+++ SudokuSolver/Reader.cs
@@ -16,6 +16,10 @@ namespace SudokuSolver
             var directory = ConfigurationManager.AppSettings["SudokuPath"];
             Console.WriteLine("Reading Sudoku files from path: {0}", directory);
             var files = Directory.GetFiles(directory, "*.txt");
+            if (files.Count()==0)
+            {
+                Console.WriteLine("No Files Found");
+            }
             foreach (var file in files)
             {
                 Console.WriteLine(file);
@@ -23,7 +27,26 @@ namespace SudokuSolver
                 if (ValidateFile(fileContent))
                 {
                     var solution = SudokuSolver.Solve(fileContent);
+                    if (solution == null)
+                    {
+                        Console.WriteLine("No Solutions found");
+                    }
+                    else
+                    {
+                        foreach (var i in solution)
+                        {
+                            foreach(var j in i)
+                            {
+                                Console.Write(j);
+                            }
+                            Console.WriteLine();
+                        }
+                    }
 
+                }
+                else
+                {
+                    Console.WriteLine("The file {0} is not in a proper 3x3 sudoku format", file);
                 }
             }
             Console.ReadKey();
--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -14,10 +14,12 @@ namespace SudokuSolver
             int[][] lastGuess = new int[9][];
             for (var i =0; i<9; i++)
             {
+                var row = new int[9];
                 for (var j= 0; j<9; j++)
                 {
-                    lastGuess[i][j] = 0;
-                }                                        
+                    row[j] = 0;
+                }
+                lastGuess[i] = row;                                      
             }
 
             Stack<GridInfo> gridHistory = new Stack<GridInfo>();
@@ -34,42 +36,89 @@ namespace SudokuSolver
                 }
                 grid[i] = row;
             }                                           
-            gridHistory.Push(new GridInfo(grid,0,0));     
+            gridHistory.Push(new GridInfo(grid,0,0));
 
             for (var i = 0; i < 9; i++)
             {
-                for (var j = 0; j < 9; j++)
+                for (var j = 0; j < 9 && !failed; j++)
                 {
+                    System.Threading.Thread.Sleep(500);
+                    Console.WriteLine(i + " " + j);
                     if (failed)
-                    {                           
+                    {
                         var gridInfo = gridHistory.Pop();
                         grid = gridInfo.grid;
                         i = gridInfo.row;
                         j = gridInfo.column - 1;
+                        failed = false;
                     }
-                    if(grid[i][j] != 0)
+                    if (grid[i][j] != 0)
                     {
                         continue;
                     }
-                    for (var guess = lastGuess[i][j]+1; guess < 9; guess++)
-                    {
-                        if(CheckIfNumberIsValid(i,j,grid, guess))
-                        {    
+                    for (var guess = lastGuess[i][j] + 1; guess < 9; guess++)
+                    {                        
+                        if (CheckIfNumberIsValid(i, j, grid, guess))
+                        {
                             grid[i][j] = guess;
-                            gridHistory.Push(new GridInfo(grid,i, j));
+                            gridHistory.Push(new GridInfo(grid, i, j));
+                            lastGuess[i][j] = guess;
                             continue;
                         }
                     }
                     failed = true;
                 }
             }
-            throw new NotImplementedException();         
+
+            if(CheckIfSudokuIsSolved(grid))
+            {
+                return grid;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private static bool CheckIfSudokuIsSolved(int[][] grid)
+        {
+            foreach (var row in grid)
+            {
+                foreach(var num in row)
+                {
+                    if (num == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
 
         public static bool CheckIfNumberIsValid(int row, int col, int[][] grid, int number)
         {
-            //foreach (in)
-            throw new NotImplementedException();
+            for(var i = 0; i < 9; i++)
+            {
+                if (grid[row][i] == number || grid [i][col] == number)
+                {
+                    return false;
+                }
+            }
+
+            var boxRow = row / 3;
+            var boxCol = col / 3;
+
+            for (var i = boxRow*3; i<(boxRow+1)*3; i++)
+            {
+                for (var j = boxCol*3; j<(boxCol+1)*3; j++)
+                {
+                    if (grid[i][j] == number)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
